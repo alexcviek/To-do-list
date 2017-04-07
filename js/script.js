a9be9d25894781dd48761b21f41748fe159b4f1f
Alex Cviek <a.i.cwiek@gmail.com>
2017-04-07 19:30:02 +0100

    created the main script

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var addItem = function(){
+	var input = document.getElementsByTagName('input')[0];
+	var newItemText = input.value;
+
+
+	var newItem = document.createElement('li');
+	newItem.innerHTML = newItemText;
+
+	document.getElementById('todo').appendChild(newItem);
+};
+document.getElementsByTagName('button')[0].addEventListener('click', addItem);
+
+
+
