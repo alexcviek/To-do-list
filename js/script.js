55b32a4abbfaa0802d2a968781c76838014c2b22
Alex Cviek <a.i.cwiek@gmail.com>
2017-04-09 17:28:48 +0100

    added cross out function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//adding an item
 var addItem = function(){
 	var input = document.getElementsByTagName('input')[0];
 	var newItemText = input.value;
@@ -6,9 +7,25 @@ var addItem = function(){
 	var newItem = document.createElement('li');
 	newItem.innerHTML = newItemText;
 
+	if (newItemText === ''){
+		alert("You must write sth!");
+	}
+	else {
 	document.getElementById('todo').appendChild(newItem);
+	}
 };
 document.getElementsByTagName('button')[0].addEventListener('click', addItem);
+
+//crossing out toggle
+var list = document.querySelector('ul');
+var crossOut = function(item){
+ if (item.target.tagName === 'LI') {
+   item.target.classList.toggle('done');
+  }
+}
+list.addEventListener('click', crossOut, false);
+
+
 
 
 
